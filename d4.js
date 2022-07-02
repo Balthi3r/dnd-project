19bf01ae6f3e728a0ac642b440b36707c022e306
Balthier <rrodrijose@gmail.com>
2022-07-02 12:07:49 -0500

    insteresting

--- d4.js
+++ d4.js
@@ -1,9 +1,13 @@
 let d4;
 
+const loadD4 = () => {
 
-document.getElementById("2ndattck").onclick = function(){
+
+document.getElementById("2ndattck").onclick = 
+function(){
 
     x = Math.floor(Math.random() * 4) + 1;
 
     document.getElementById("d4label").innerHTML = x;
+}
 }
--- index.js
+++ index.js
@@ -1,7 +1,50 @@
-let Char={
-    name: '',
-    class: 'Monk',
-    hp: 10,
-    dex:19,
-    wisdom:17,
+const textElement = document.getElementById('text')
+const routeGridElement = document.getElementById('route-grid')
+
+let state = {}
+
+
+
+
+function showTextNode(textNodeIndex){
+    const textNode= textNodes.find(textNode => textNode.id === textNodeIndex)
+    textElement.innerText= textNode.text 
+    while (routeGridElement.firstChild){
+        routeGridElement.removeChild(routeGridElement.firstChild)
+    }
+    textNode.options.forEach(option => {
+        if (showOption(option)){
+            const button= document.createElement('button')
+            button.innerText=option.text
+            button.classList.add('b')
+        }
+    })
 }
+
+function selectOption(option){
+    return true
+
+}
+
+const textNodes=[
+    {
+        id: 1,
+        text: 'Its lunch time and you decide to get something to eat when you see a glorious $100 bill in the middle of the road.',
+        options: [
+            {
+                text: 'take the $100',
+                setState: {$100bill: true},
+                nextText:2
+            },
+            {
+                text: 'leave the $100',
+                nextText: 2
+            }
+
+        ]
+    },
+    {
+        id: 2,
+    }
+]
+showTextNode(1)
